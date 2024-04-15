4ff453d7e9555247ddca055251019ce584276008
Katelyn Rohrer <k.rohrer03@gmail.com>
2024-04-15 15:26:11 -0700

    wiki page parsing works

--- src/main/java/model/WikiPage.java
+++ src/main/java/model/WikiPage.java
@@ -1,17 +1,29 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WikiPage {
     private final ArrayList<Page> pages;
 
     public WikiPage(String contents) {
-        String[] pageStrs = contents.split("\\[\\[");
+        // Using regex to find the page sections. This also makes it
+        // lazy evaluation, so it should be a bit more memory efficient.
+        String regex =  "\\[\\[(.*?)]]\n" +       // match the title
+                        "(.*?)" +                 // and all text in the middle
+                        "(?=\\[\\[(.*?)]]\n|$)";  // until the next title match (or EOF)
+        Pattern pattern = Pattern.compile(regex, Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(contents);
+
+
         pages = new ArrayList<>();
 
-        for (String pageStr : pageStrs) {
-            Page p = new Page(pageStr);
+        while (matcher.find()) {
+            String pageText = matcher.group(0);
+            Page p = new Page(pageText);
             pages.add(p);
+            return;
         }
     }
 
