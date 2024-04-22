4c8ad67b0523c1767b2a60bcffd34212817cd767
CG020 <gmills020@gmail.com>
2024-04-22 15:27:45 -0700

    query parsing multifield

--- src/main/java/model/Query.java
+++ src/main/java/model/Query.java
@@ -4,25 +4,46 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.search.similarities.ClassicSimilarity;
 import org.apache.lucene.store.Directory;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
 
 public class Query {
     Directory index;
     StandardAnalyzer analyzer;
+    MultiFieldQueryParser parser;
+    HashMap<String, Float> boosts;
+
 
     public Query(Directory index, StandardAnalyzer analyzer) {
         this.index = index;
         this.analyzer = analyzer;
+    }
+
+
+    // this is where we will boost nujmbers for the doc fields - creates multifield parser
+    public void assignBoosts(HashMap<String, Float> boosts)  {
+        boosts = new HashMap<>();
+        boosts.put("categories", 2.0f);
+        boosts.put("summary", 1.5f);
+        boosts.put("bodyText", 1.5f);
+        boosts.put("headers", 0.5f);
+
+        this.parser = new MultiFieldQueryParser(
+            new String[] {"categories", "summary", "bodyText", "headers"},
+            analyzer,
+            boosts
+        );
     }
 
 
@@ -36,30 +57,50 @@ public class Query {
     public List<ResultClass> runQuery(String queryStr) throws IOException {
         // make the query parser
         org.apache.lucene.search.Query q;
-        try { q = new QueryParser("text", analyzer).parse(queryStr); }
+        // try { q = new QueryParser("text", analyzer).parse(queryStr); }
+        // catch (ParseException e) { throw new RuntimeException(e); }
+        
+
+        assignBoosts(this.boosts);
+        try {q = parser.parse(queryStr); }
         catch (ParseException e) { throw new RuntimeException(e); }
 
-        // search
         int hitsPerPage = 10;
+
+        String wikiPath = "sample";
         IndexReader reader = DirectoryReader.open(index);
+        // serilized 
 
         IndexSearcher searcher = new IndexSearcher(reader);
 
-        TopDocs docs = searcher.search(q, hitsPerPage);
-        ScoreDoc[] hits = docs.scoreDocs;
+        TopDocs pages = searcher.search(q, hitsPerPage);
+        ScoreDoc[] hits = pages.scoreDocs;
 
         // add to score
         System.out.format("Query '%s' returned:\n", queryStr);
         List<ResultClass> ans = new ArrayList<>();
         for (ScoreDoc hit : hits) {
             Document d = searcher.doc(hit.doc);
-            ResultClass docObj = new ResultClass();
-            docObj.DocName = d;
-            docObj.docScore = hit.score;
-            ans.add(docObj);
+            ResultClass page = new ResultClass();
+            page.DocName = d;
+            page.docScore = hit.score;
+            ans.add(page);
             System.out.format("\t%s: %f\n", d.get("docid"), hit.score);
         }
         reader.close();
         return ans;
+    }
+
+    public static void main(String[] args ) {
+        // mvn exec:java -D"exec.mainClass=model.Index"
+        String wikiPath = "sample";
+        try {
+            Index indexer = new Index(wikiPath);
+            Query q = new Query(indexer.index, new StandardAnalyzer());
+            try {
+                List<ResultClass> ans = q.runQuery("The dominant paper in our nation's capital, it's among the top 10 U.S. papers in circulation");
+            } catch (IOException e) { throw new RuntimeException(e); }
+        } catch (IOException e) { e.printStackTrace();}
+
     }
 }
