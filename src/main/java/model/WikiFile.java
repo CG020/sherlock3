0c9385517ee4876800997b326b97d353aef5e5ec
Katelyn Rohrer <k.rohrer03@gmail.com>
2024-04-29 12:56:56 -0700

    commenting wikifile

--- src/main/java/model/WikiFile.java
+++ src/main/java/model/WikiFile.java
@@ -1,13 +1,30 @@
+/*
+ * Katelyn Rohrer, Lydia Dufek, Camila Grubb
+ * CSC 483/583
+ * Defines the WikiFile class, which manages a single file containing several
+ * wikipedia pages within it.
+ */
+
 package model;
 
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+/**
+ * The WikiFile class is used to manage wikipedia files containing several
+ * wiki pages and creates the corresponding Page objects for each page
+ * parsed out.
+ */
 public class WikiFile {
     private final ArrayList<Page> pages;
 
-
+    /**
+     * Creates the WikiFile object and parses through each of the pages using
+     * regex, adding each of the pages to the pages attribute as they're read in.
+     * @param contents The contents of the entire file
+     */
     public WikiFile(String contents) {
         // Using regex to find the page sections. This also makes it
         // lazy evaluation, so it should be a bit more memory efficient.
@@ -22,6 +39,7 @@ public class WikiFile {
 
         pages = new ArrayList<>();
 
+        // until there are no more matches, create the correlating page type and add it to pages
         while (matcher.find()) {
             String title = matcher.group(1);
             String pageText = matcher.group(0);
@@ -30,6 +48,15 @@ public class WikiFile {
         }
     }
 
+    /**
+     * Sorts a page as a redirect page, unclear page, or normal page
+     * based on the contents of the document. The corresponding Page subclass
+     * is returned.
+     * @param title The title of the wikipedia page
+     * @param contents All of the contents of the wikipedia page
+     * @return Either an UnclearPage, RedirectPage, or NormalPage object,
+     * determined by which type of page the string represents.
+     */
     private Page createCorrectPageType(String title, String contents) {
         String unclearSearchPhrase = title + " may refer to:";
         String redirectSearchPhrase = "#REDIRECT";
@@ -43,16 +70,21 @@ public class WikiFile {
         }
     }
 
-
+    /**
+     * Getter for pages
+     * @return Arraylist of Page objects for the pages within the file
+     */
     public ArrayList<Page> getPages() {
         return pages;
     }
 
+    /**
+     * Clears out all of the contents for all of the pages within this file.
+     */
     public void clearAllContents() {
         for (Page p: pages) {
             p.clearContents();
         }
     }
-
 
 }
